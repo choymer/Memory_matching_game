fb1ac515525e135adf2b889a9a4201638f3a4732
Mercedes <mercedeschoy@gmail.com>
2021-06-28 15:47:07 +0100

    Adding functionality on the cards

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const aceOfSpadesOne = document.getElementById('ace-spades-one');
 const aceOfSpadesTwo = document.getElementById('ace-spades-two');
-
+let counter = 0;
 
 function revealCard(id, card) {
-
+counter++
 
   if(document.getElementById(id).getAttribute('src') == "./card-back.png") {
     document.getElementById(id).src = card;
+    document.getElementById(id).classList.add('same_card')
   }
+
+  
+  if (counter === 2) {
+    if(document.querySelectorAll('.same_card')[0].src !== document.querySelectorAll('.same_card')[1].src) {
+
+  setTimeout(function() {
+
+    document.querySelectorAll('.same_card')[0].src = './card-back.png'
+    document.querySelectorAll('.same_card')[1].src = './card-back.png'
+
+    function removeClasses() {
+      document.querySelectorAll('.same_card')[0].classList.remove('same_card');
+      document.querySelectorAll('.same_card')[0].classList.remove('same_card');   
+    }
+    removeClasses()
+}, 3000);
+
+counter = 0;
+    
+    }
+
+  }
+
 
   
 }
